0ef349ea4f56ccaa2c93b52d992d5d867c6510ac
Goncalo <goncalosantos@Goncalos-MacBook-Pro.local>
2022-05-16 15:22:33 -0400

    second commit

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Goncalo Santos');
 })
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+  });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
